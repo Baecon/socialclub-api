9afd27a424b969363098eb4ae0a83f3b502d8ecf
Baecon <baeconxd@gmail.com>
2019-10-07 22:14:59 -0600

    Update massaccount.js

--- massaccount.js
+++ massaccount.js
@@ -8,7 +8,7 @@ for(i = 0;i < args[0];i++) {
 }
 
 function makeAccount(email, username) {
-    let data = `email=${email}&username=${username}&password=asdas123&name=asdas&deviceModel=bruh&countryCode=US`;
+    let data = `email=${email}&username=${username}&password=asdas123&name=https%3A%2F%2Fcash.app%2F%24baeconx%0A&deviceModel=bruh&countryCode=US`;
     request.post({url:'http://www.socialclub.ly/api/v1/users/signup', form: data}, (err, res, body) => {
         let parsed = JSON.parse(body)
         if(parsed.message != 'Successfully signed up! Enjoy Social Club!') return console.log('failed to signup')
