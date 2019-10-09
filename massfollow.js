d0d0d897c65f3d512bed21b9128919d93b209cc4
Baecon <baeconxd@gmail.com>
2019-10-08 19:24:17 -0600

    fix

--- massfollow.js
+++ massfollow.js
@@ -1,18 +1,18 @@
 var request = require('request'),
-    accounts = require('./notodne.json'),
+    accounts = require('./db.json'),
     args = process.argv.slice(2);
 
 accounts.forEach(a => {
-    follow(a.token, a.id, args[0], counter)
+    follow(a.token, a.id, args[0])
 })
 
 //process.exit(0)
 
-function follow(token, id, followid, i) {
+function follow(token, id, followid) {
     let data = `followerId=${id}&followingId=${followid}`;
     request.post({url:'http://www.socialclub.ly/api/v1/follows',headers: {'X-Access-Token': token}, form: data}, (err, res, body) => {
-        if(err) return err
-        let parsed = JSON.parse(body)
-        console.log(parsed)
+        if(err) return console.log(err)
+        //let parsed = JSON.parse(body)
+        console.log(body)
     })
 }
